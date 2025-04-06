c9a6fcc2686b1ffd5560775651c6538aa906ffc2
NgTMDuc <ducntm23052002@gmail.com>
2025-04-06 10:09:15 +0700

    Push

---
+++ finetune/training.py
@@ -0,0 +1,10 @@
+from cosmos1 import CausalContinuousVideoTokenizer
+CONFIG = {
+    'z_channels': 512,
+    'zip_factor': 8,
+    'latent_channels': 512,
+    'name': 'CausalContinuousVideoTokenizer',
+    'encoder': 'BASE',
+    'decoder': 'BASE',
+    
+}
